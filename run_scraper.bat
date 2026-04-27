4e3744f200c245d66a00e2de7a6ae8925f0a6cd0
dadezeeuw <dadezeeuw@psara.com>
2026-04-26 20:59:01 -0400

    Merge branch 'main' of https://github.com/dadezeeuw/smps-events

--- run_scraper.bat
+++ run_scraper.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 cd /d "C:\users\dadezeeuw\Documents\smps-events"
 
@@ -12,19 +11,4 @@ if errorlevel 1 (
 git add docs/events.json
 git commit -m "Update scraped SMPS events"
 
-=======
-@echo off
-cd /d "C:\users\dadezeeuw\Documents\smps-events"
-
-python scrape_events.py
-
-if errorlevel 1 (
-    echo Scraper failed. Not committing changes.
-    exit /b 1
-)
-
-git add docs/events.json
-git commit -m "Update scraped SMPS events"
-
->>>>>>> 8a5918c7b5f5c13ce10933c5da3728aeb82cc922
 git push
